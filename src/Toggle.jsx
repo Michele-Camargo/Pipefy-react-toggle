e835ad51f208817be67ca922aaec494039081a7f
Michele.Camargo <mi.farias98@gmail.com>
2022-07-21 15:50:07 -0300

    added color intext

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -4,17 +4,20 @@ import { Switch } from "@material-ui/core";
 export function Toggle({ title }) {
   const [active, setActive] = useState(true);
   let text;
+  let color;
 
   if (active) {
     text = "On fire for the party!";
+    color = "red";
   } else {
     text = "Off to sleep.";
+    color = "blue";
   }
   return (
     <div>
       <h1>{title}</h1>
       <Switch checked={active} onChange={() => setActive(!active)} />
-      <span>{text}</span>
+      <span style={{ color: color }}>{text}</span>
     </div>
   );
 }
